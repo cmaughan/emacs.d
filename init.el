2a0b908b72ff7bda3dbd7a03467bf8ed0bd83183
Chris Maughan <mornymorny@gmail.com>
2016-03-07 12:20:26 +0000

    Neotree

--- init.el
+++ init.el
@@ -27,6 +27,14 @@
 (add-to-list 'load-path "~/.emacs.d/neotree")
 (require 'neotree)
 (global-set-key [f8] 'neotree-toggle)
+(add-hook 'neotree-mode-hook
+            (lambda ()
+              (define-key evil-normal-state-local-map (kbd "TAB") 'neotree-enter)
+              (define-key evil-normal-state-local-map (kbd "SPC") 'neotree-enter)
+              (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
+              (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)))
+; Open at current file dir when tree is opened
+(setq neo-smart-open t)
 
 ; Load configuration modules
 (load-user-file "evil.el")
